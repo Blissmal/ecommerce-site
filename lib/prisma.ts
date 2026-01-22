68f7297e39f90393aa1bfc05d934ddc8b13713f1
Blissmal <malutibethuel@gmail.com>
2026-01-23 02:24:20 +0300

    updated prisma configs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from "@/generated/prisma";
-
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+// 1. Setup the database driver (outside the client)
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+// 2. Pass the adapter to the PrismaClient constructor
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ?? 
+  new PrismaClient({ 
+    adapter // This replaces the implicit URL lookup
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
